68114ab7fc58f430d3e58a84f65b014d70c0b40f
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-18 02:10:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,7 @@
 # # code here!
 # School
 #   ::new
-#     has an empty roster when initialized (FAILED - 1)
-#   #add_student
-#     is able to add a student (FAILED - 2)
-#     is able to add multiple students to a class (grade) (FAILED - 3)
-#     is able to add students to different grades (FAILED - 4)
-#   #grade
-#     is able to retreive students from a grade (FAILED - 5)
-#   #sort
-#     is able to sort the students (FAILED - 6)
+#     has an empty roster when initialized
 
 require 'pry'
 
@@ -24,8 +16,13 @@ class School
     @roster = {}
   end
 
+  # #add_student
+  # is able to add a student
+  # is able to add multiple students to a class (grade)
+  # is able to add students to different grades
+
   def add_student(student_name, grade)
-    if @roster[grade] 
+    if @roster[grade]
       @roster[grade] << student_name
     else
       @roster[grade] = []
