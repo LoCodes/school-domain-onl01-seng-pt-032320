fd6c1aa163e85a7b0052f42d5c1c0aebe4f61cdd
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-18 01:27:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+# # code here!
+# School
+#   ::new
+#     has an empty roster when initialized (FAILED - 1)
+#   #add_student
+#     is able to add a student (FAILED - 2)
+#     is able to add multiple students to a class (grade) (FAILED - 3)
+#     is able to add students to different grades (FAILED - 4)
+#   #grade
+#     is able to retreive students from a grade (FAILED - 5)
+#   #sort
+#     is able to sort the students (FAILED - 6)
+
+
+class School
+  attr_accessor :add_student :grade :sort
+  attr_reader :roster 
+
+  ROSTER = []
+
+  def roster(roster)
+    @roster = roster 
+  end 
+end 
